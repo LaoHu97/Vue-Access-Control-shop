47a671aeee11ab51e716b33d62418420133a92a6
LaoHu97 <962271461@qq.com>
2018-11-22 10:29:45 +0800

    2018-11-22

--- src/components/tinymce/plugins.js
+++
@@ -1,3 +0,0 @@
-const plugins = [ "advlist autolink lists link image charmap print preview anchor searchreplace visualblocks code fullscreen insertdatetime contextmenu paste textcolor" ]
-
-export default plugins
--- src/components/tinymce/toolbar.js
+++
@@ -1,3 +0,0 @@
-const toolbar = "forecolor backcolor emoticons | fontsizeselect | insertfile undo redo | styleselect | bold italic | alignleft aligncenter alignright alignjustify | bullist numlist outdent indent | link image "
-
-export default toolbar
